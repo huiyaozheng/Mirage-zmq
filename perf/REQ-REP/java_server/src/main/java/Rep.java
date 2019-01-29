ea582459280e398f62f1440dfeae18c6018fc758
Huiyao Zheng <huiyaozheng@gmail.com>
2019-01-29 11:54:37 +0000

    trial of jeromq

---
+++ perf/REQ-REP/java_server/src/main/java/Rep.java
@@ -0,0 +1,28 @@
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+
+public class Rep
+{
+    public static void main(String[] args) throws Exception
+    {
+        try (ZContext context = new ZContext()) {
+            // Socket to talk to clients
+            ZMQ.Socket socket = context.createSocket(ZMQ.REP);
+            socket.bind("tcp://*:5555");
+
+            while (!Thread.currentThread().isInterrupted()) {
+                // Block until a message is received
+                byte[] reply = socket.recv(0);
+
+                // Print the message
+                System.out.println(
+                        "Received: [" + new String(reply, ZMQ.CHARSET) + "]"
+                );
+
+                // Send a response
+                String response = "Hello, world!";
+                socket.send(response.getBytes(ZMQ.CHARSET), 0);
+            }
+        }
+    }
+}
