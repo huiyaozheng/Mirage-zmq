b9b8031fe9a69763921554065f9867b2bece7b0c
Huiyao Zheng <huiyaozheng@gmail.com>
2019-02-04 10:41:07 +0000

    perf client done

--- perf/REQ-REP/client/client.cpp
+++ perf/REQ-REP/client/client.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <string>
 #include <zmq.hpp>
 #include <chrono>
 class Timer {
@@ -30,34 +32,49 @@ class Timer {
   bool m_bRunning = false;
 };
 
-int main() {
-  //  Prepare our context and socket
+class Writer {
+  std::ofstream out;
+public:
+  Writer(std::string filename) {
+    out.open (filename+".csv");
+  }
+
+  void write(std::string line) {
+    out << line << '\n';
+  }
+
+  ~Writer() {
+    out.close();
+  }
+};
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "A CSV filename is needed" << std::endl;
+    return 0;
+  }
+  Writer w(argv[1]);
+  
+  w.write("Req no., latency (microsecond)");
   zmq::context_t context(1);
   zmq::socket_t socket(context, ZMQ_REQ);
-
-  std::cout << "Connecting to hello world server…" << std::endl;
+  std::cout << "Connecting to server…" << std::endl;
   socket.connect("tcp://localhost:5555");
-
   Timer timer;
-
-  //  Do 10 requests, waiting each time for a response
-  for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
-    zmq::message_t request(5);
-    memcpy(request.data(), "Hello", 5);
-    std::cout << "Sending Hello " << request_nbr << "…" << std::endl;
-    
+  const int REQ_NO = 10000;
+  for (int request_nbr = 0; request_nbr != REQ_NO; request_nbr++) {
+    zmq::message_t request(7);
+    memcpy(request.data(), "Request", 7);
+    std::cout << "Sending request " << request_nbr << "…" << std::endl;
     timer.start();
     socket.send(request);
-
-    //  Get the reply.
     zmq::message_t reply;
     socket.recv(&reply);
-    
     timer.stop();
-
-    std::cout << "Microseconds: " << timer.elapsedMicroseconds() << std::endl;
-
-    std::cout << "Received World " << request_nbr << std::endl;
+    double latency = timer.elapsedMicroseconds();
+    //std::cout << "Microseconds: " << timer.elapsedMicroseconds() << std::endl;
+    w.write(std::to_string(request_nbr) + ',' + std::to_string(latency));
+    std::cout << "Received reply " << request_nbr << std::endl;
   }
   return 0;
 }
