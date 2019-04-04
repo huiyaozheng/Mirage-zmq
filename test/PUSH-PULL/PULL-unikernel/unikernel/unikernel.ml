ce284bd7def3ebb2bf2f7a98bacefca63a4d1129
Huiyao Zheng <huiyaozheng@gmail.com>
2019-04-04 21:55:57 +0100

    PUSH-PULL test passed

--- test/PUSH-PULL/PULL-unikernel/unikernel/unikernel.ml
+++ test/PUSH-PULL/PULL-unikernel/unikernel/unikernel.ml
@@ -8,7 +8,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
             Socket.connect socket "127.0.0.1" 5556 s >>= fun () ->
             let rec read_and_print () =
                 Socket.recv socket >>= fun msg -> match msg with
-                | [Data(msg)] -> Logs.info (fun f -> f "Received msg: %s\n" msg);
+                | Data(msg) -> Logs.info (fun f -> f "Received msg: %s\n" msg);
                         read_and_print ()
                 | _ -> Logs.info (fun f -> f "Unexpected msg received\n");  Lwt.return_unit
             in Logs.info (fun f -> f "Started socket\n");  read_and_print ()
