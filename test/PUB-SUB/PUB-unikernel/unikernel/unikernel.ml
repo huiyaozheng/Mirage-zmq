2a045b72a62597864021b517b89e9fbfdd30bee3
Huiyao Zheng <huiyaozheng@gmail.com>
2019-04-06 16:32:37 +0100

    PUB-SUB regression passed

--- test/PUB-SUB/PUB-unikernel/unikernel/unikernel.ml
+++ test/PUB-SUB/PUB-unikernel/unikernel/unikernel.ml
@@ -5,10 +5,11 @@ module Main (S: Mirage_stack_lwt.V4) = struct
         let context = Context.create_context () in
         let module Socket = Socket_tcp (S) in
         let socket = Socket.create_socket context PUB in
+            Socket.set_outgoing_queue_size socket 10;
             Socket.bind socket 5556 s;
             let rec publish content =
             try
-                Socket.send socket [Data(content)];
+                Socket.send socket (Data(content));
                 Logs.info (fun f -> f "work item sent");
                 Lwt.pause () >>= fun () -> publish content
             with No_Available_Peers -> Lwt.pause () >>= fun () -> publish content
