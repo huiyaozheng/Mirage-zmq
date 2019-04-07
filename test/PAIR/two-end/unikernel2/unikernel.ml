fb52e5daad9187acd2f74cea9f7d3580265200bf
Huiyao Zheng <huiyaozheng@gmail.com>
2019-04-07 09:43:38 +0100

    two-end PAIR passed

--- test/PAIR/two-end/unikernel2/unikernel.ml
+++ test/PAIR/two-end/unikernel2/unikernel.ml
@@ -5,7 +5,7 @@ module Main (S: Mirage_stack_lwt.V4) = struct
         let context = Context.create_context () in
         let module Socket = Socket_tcp (S) in
         let socket = Socket.create_socket context PAIR in
-            Socket.bind socket 5556 s;
+            Socket.connect socket "127.0.0.1" 5556 s >>= fun () ->
             let rec send content =
             try
                 Socket.send socket (Data(content));
