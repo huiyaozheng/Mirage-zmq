b097c084a334a2234695421990443ca385557ffc
Huiyao Zheng <huiyaozheng@gmail.com>
2019-04-11 23:30:35 +0100

    fixed java build scripts

--- perf/REQ-REP/cpp_server/server.cpp
+++ perf/REQ-REP/cpp_server/server.cpp
@@ -1,7 +1,12 @@
 #include <zmq.hpp>
 #include <string>
 #include <iostream>
-
+auto msg_length = 255;
+auto msg =
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 int main () {
     //  Prepare our context and socket
     zmq::context_t context (1);
@@ -11,8 +16,8 @@ int main () {
         zmq::message_t request;
         //  Wait for next request from client
         socket.recv (&request);
-        zmq::message_t reply (5);
-        memcpy (reply.data (), "Reply", 5);
+        zmq::message_t reply(msg_length);
+        memcpy(reply.data(), msg, msg_length);
         socket.send (reply);
     }
     return 0;
---
+++ perf/REQ-REP/java_server/run_server.sh
@@ -0,0 +1,2 @@
+cd ./target
+java -jar rep-1-jar-with-dependencies.jar Rep
