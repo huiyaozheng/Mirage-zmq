d292f8870a151364d6625b2f9d679d8313bb101e
Huiyao Zheng <huiyaozheng@gmail.com>
2019-04-25 19:11:01 +0100

    added plain unikernel client

--- test/REP/rep.py
+++ test/REP/rep.py
@@ -5,7 +5,7 @@ context = zmq.Context()
 #  Socket to talk to server
 print("Starting server...")
 socket = context.socket(zmq.REP)
-socket.bind("tcp://127.0.0.1:5556")
+socket.bind("tcp://127.0.0.1:5555")
 while True:
     msg = socket.recv_string()
     print("Received " + msg)
