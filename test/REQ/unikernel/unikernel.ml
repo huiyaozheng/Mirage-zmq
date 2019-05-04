eee3219916467235917d0396c4b976cdc4746a2e
Huiyao Zheng <huiyaozheng@gmail.com>
2019-05-04 21:08:19 +0100

    updated req unikernel

--- test/REQ/unikernel/unikernel.ml
+++ test/REQ/unikernel/unikernel.ml
@@ -6,12 +6,12 @@ module Main (S: Mirage_stack_lwt.V4) = struct
         let module Socket = Mirage_zmq.Socket_tcp (S) in
         let socket = Socket.create_socket context REQ in
             Socket.connect socket "127.0.0.1" 5556 s >>= fun () ->
-            let read_and_print : unit Lwt.t =
+            let read_and_print () =
                 Socket.send socket (Data("World")) >>= fun () ->
                 Socket.recv socket >>= fun msg -> match msg with
                 | Data(msg) -> Logs.info (fun f -> f "Received msg: %s\n" msg); 
                         Lwt.return_unit
                 | _ -> Lwt.return_unit
             in  Logs.info (fun f -> f "Started socket \n"); 
-                read_and_print
+                read_and_print ()
 end
